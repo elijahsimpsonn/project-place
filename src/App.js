c939dcb61815194cd6131398448d162e95963c34
Elijah Simpson <elijahsimpson1991@gmail.com>
2021-02-17 22:14:11 -0500

    fixed bug in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Intro from './components/Intro'
-import About from './components/About'
-import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 
