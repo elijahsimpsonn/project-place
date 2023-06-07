7a4720493172ab76a38c88b14ce68aa2cd269eaf
Elijah Simpson <elijahsimpson1991@gmail.com>
2023-06-07 11:40:28 -0400

    added more content to the about section

--- src/components/About.js
+++ src/components/About.js
@@ -7,13 +7,20 @@ export default function About() {
     <AboutDiv>
       <AboutTitle>About me</AboutTitle>
       <MainAboutText>
-      <p>Product Manager | Creating User-Centric, Worldclass Products <br /> Promoting Agile Methodolgies & Workflows | Establishing DevOps Culture & Best Practices</p>
+        <p>Product Manager | Creating User-Centric, Worldclass Products<br />Promoting Agile Methodolgies & Workflows | Establishing DevOps Culture & Best Practices</p>
       </MainAboutText>
       <AboutText>
-      Ever since I discovered Product Management, I have been obsessed with creating and delivering world-class products with outstanding customer experiences and value. Alongside helping others grow personally and professionally, it is what drives me to go the extra mile every day. I believe in working hard, treating people with respect, and focusing on a defined vision.
+        <p>Ever since I discovered Product Management, I have been obsessed with creating and delivering world-class products with outstanding customer experiences and value. 
+        Alongside helping others grow personally and professionally, it is what drives me to go the extra mile every day. I believe in working hard, treating people with respect, 
+        and focusing on a defined vision.</p>
       </AboutText>
       <AboutText>
-      <p>If you like what you see here, I would love to connect with you about an open position in your organization, a chance to partner with you on an open-source project, or just talk about development in general. I always enjoy talking about the industry and new technologies. Drop me a line at any of the contact links at the bottom of the page.</p>
+        <p>I also continue to love software development and DevSecOps culture and implementation. Software development gives us the ability to turn dreams into reality, and as technologies
+          continue to improve to scope of those dreams grow.</p>
+      </AboutText>
+      <AboutText>
+        <p>If you like what you see here, I would love to connect with you about an open position in your organization, a chance to partner with you on an open-source project, 
+        or just talk about product, development, or DevSecOps in general. Drop me a line at any of the contact links at the bottom of the page.</p>
       </AboutText>
       <Skills>
         <SkillsDiv>
